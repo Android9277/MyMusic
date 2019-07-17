787955bb76bbf9ddfb02b71b97ce5e9be05b1121
danqing <s928042740@163.com>
2019-07-17 09:45:31 +0800

    new class

---
+++ app/src/main/java/com/example/mymusic/helps/Test.java
@@ -0,0 +1,32 @@
+package com.example.mymusic.helps;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+/**
+ * Created by 孙丹青 on 2019/7/17.
+ * 作用:
+ */
+
+public class Test extends BaseAdapter {
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        return null;
+    }
+}
